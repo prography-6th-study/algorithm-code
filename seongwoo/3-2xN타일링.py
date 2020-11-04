bfc80fd33df71b748e73aee41d1ea3727d67e13c
nuatmochoi <tjddn8770@naver.com>
2020-11-04 14:56:50 +0900

    Add: kakao level-3 probs

---
+++ seongwoo/3-2xN타일링.py
@@ -0,0 +1,8 @@
+def solution(n):
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3, n + 1):
+        dp[i] = dp[i - 2] + dp[i - 1]
+        dp[i] %= 1000000007
+    return dp[-1]
